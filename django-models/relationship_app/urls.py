9544f0f93010c09a9853be0ea2969067d7434ef0
medob6 <mohamedboussetta142@gmail.com>
2024-09-07 19:42:59 +0100

    hi

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import list_books
-from .views import LibraryDetailView, LoginView, LogoutView , admin_view , librarian_view , member_view
+from .views import LibraryDetailView, LoginView, LogoutView , admin_view , librarian_view , member_view , add_book , edit_book , delete_book
 from . import views
 
 urlpatterns = [
@@ -28,4 +28,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,11 +1,10 @@
 # relationship_app/views.py
-from django.contrib.auth import login as auth_login
-from django.contrib.auth import logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login , logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm , UserCreationForm
+from django.contrib.auth.decorators import permission_required , user_passes_test
 
 # relationship_app/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from django.views import View
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import user_passes_test
@@ -13,7 +12,7 @@ from django.contrib.auth.decorators import user_passes_test
 # from .forms import BookForm
 from .models import Book
 from .models import Library
-from .models import UserProfile
+from .forms import BookForm
 
 # Create your views here.
 
@@ -103,3 +102,37 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+# Add Book View
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+# Edit Book View
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form, 'book': book})
+
+# Delete Book View
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'delete_book.html', {'book': book})
