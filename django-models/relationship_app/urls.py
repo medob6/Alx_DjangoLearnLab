4b86b1ddaf8231a65ea83e8c070825123f4916dc
medob6 <mohamedboussetta142@gmail.com>
2024-09-07 19:44:58 +0100

    hi

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/', edit_book, name='edit_book'),
+    path('delete_book/', delete_book, name='delete_book'),
 ]
