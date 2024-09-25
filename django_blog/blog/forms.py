bef4367ac0c17d73e054af3a83e35e0deb7027af
medob6 <mohamedboussetta142@gmail.com>
2024-09-25 17:16:00 +0100

    i added update url

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -29,4 +29,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
-
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -20,4 +20,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+class Comment(model.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now) # this timezone.now modul is to add automaticly
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
