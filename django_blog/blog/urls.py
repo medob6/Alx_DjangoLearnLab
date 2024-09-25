150fae49ed7adadb318b72f546635ea43c9e475c
medob6 <mohamedboussetta142@gmail.com>
2024-09-25 17:31:40 +0100

    add urls for comment views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='about'),
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-edit'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
